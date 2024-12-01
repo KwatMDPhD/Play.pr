898252de74b771f77b35d01b3a45d2182f23277c
KwatMDPhD <kwat.me@icloud.com>
2024-11-30 19:58:13 -0700

    Joined.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,21 @@ using Test: @test
 
 # ----------------------------------------------------------------------------------------------- #
 
+using DataFrames
+
+using Omics
+
 # ---- #
+
+da = pkgdir(DataIntegration, "data")
+
+e1 = Omics.Table.rea(joinpath(da, "GSE18520.tsv"), )
+
+e2 = Omics.Table.rea(joinpath(da, "GSE66957.tsv"), )
+
+e3 = Omics.Table.rea(joinpath(da, "GSE69428.tsv"), )
+
+# ---- #
+
+ex = innerjoin(e1, e2, e3; on = "gene_symbol")
+
