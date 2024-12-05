a9b60178127b3182deb803e680c000db378ff978
KwatMDPhD <kwat.me@icloud.com>
2024-12-05 00:28:14 -0800

    Fixed timeline.

--- pycombat.py
+++ pycombat.py
@@ -26,11 +26,11 @@ dataset_3 = pd.read_pickle("data/GSE69428.pickle")
 
 # ---- #
 
-#dataset_1.to_csv("data/GSE18520.tsv", sep="\t")
-#
-#dataset_2.to_csv("data/GSE66957.tsv", sep="\t")
-#
-#dataset_3.to_csv("data/GSE69428.tsv", sep="\t")
+dataset_1.to_csv("data/GSE18520.tsv", sep="\t")
+
+dataset_2.to_csv("data/GSE66957.tsv", sep="\t")
+
+dataset_3.to_csv("data/GSE69428.tsv", sep="\t")
 
 # ---- #
 
